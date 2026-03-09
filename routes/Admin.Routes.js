c90396be5c9118e08aed5d5415e3596f0adceda9
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-03-09 11:54:15 +0500

    Created this admin route
    which is work as a mini server for all the admin related endpoints

--- routes/Admin.Routes.js
+++ routes/Admin.Routes.js
@@ -7,11 +7,16 @@ import adminPanel from '../controllers/adminPanel.Controller.js'
 import { approveOrRejectLeave } from '../controllers/approveOrRejectLeave.Controller.js'
 const adminRouter = express.Router();
 
+
+// Admin registration route
 adminRouter.post('/admin-registration', adminController);
 
+// Admin login route
 adminRouter.post('/admin-login', adminLoginValidation, adminLogin);
 
+// Admin panel route (protected)
 adminRouter.get('/admin-panel', adminAuthenticationHandler, adminPanel);
 
+// Approve or reject leave request route (protected)
 adminRouter.post('/admin-approve-or-reject-leave', adminAuthenticationHandler, approveOrRejectLeave);
 export default adminRouter;
