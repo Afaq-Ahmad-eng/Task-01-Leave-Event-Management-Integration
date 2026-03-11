689ff276254f525d5f388c23af0c85b8bb1565c1
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-03-11 10:10:28 +0500

    Added the Module 2 routes into the app.js
    so, we can integrate all the parts of the system and the the system serve as whole system

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import router from './routes/Module-1-Leave-Management/employeeLeave.Routes.js';
 import employeeRegistrationRoute from './routes/Module-1-Leave-Management/employeeRegistration.Routes.js';
 import { connectDB } from './config/database.js';
 import adminRouter from './routes/Module-1-Leave-Management/Admin.Routes.js';
+import eventCreateByAdminRouter from './routes/Module-2-Event-Management/EventCreateByAdmin.Routes.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -12,6 +13,7 @@ connectDB()
 app.use('/api',router);
 app.use('/api',employeeRegistrationRoute);
 app.use('/api',adminRouter);
+app.use('/api', eventCreateByAdminRouter);
 
 
 export default app;
